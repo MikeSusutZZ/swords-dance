66735d14f4393fb686e927b58ca8ab4f34867c70
MikeSusutZZ <mike.susut@gmail.com>
2024-05-11 15:52:30 -0700

    updated port for deployment

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { generateDeck, pickRandomCards } = require('./deck')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 let turn = Math.ceil(Math.random() * 2);
 
